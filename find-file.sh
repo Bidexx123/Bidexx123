91f4e1314fee55fb568542b8946fe14f3e876d5e
Bidexx123 <138574802+Bidexx123@users.noreply.github.com>
2024-02-28 21:34:08 +0100

    to find write permission

--- find-file.sh
+++ find-file.sh
@@ -3,9 +3,13 @@
 echo -e "Type the name of the file : \c"
 read file_name
 
-if [ $file_name ]
+if [-f $file_name ]
 then
-    echo "file found"
+    if [ -w $file_name ]
+    echo "Type some text here, press ctrl+q to quit"
+    cat >>$file_name
+    else 
+    echo "The file does not have write permission"
 else
-    echo "file not found"
+    echo "file doesnnot exist"
 fi
